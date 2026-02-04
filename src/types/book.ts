5de7862b965d5c826daded53010485779dbc55bc
longhousepress <info@longhousepress.org>
2026-02-04 16:34:15 +0000

    Updated front end to make use of better metadata

--- src/types/book.ts
+++ src/types/book.ts
@@ -1,17 +1,39 @@
+export interface Price {
+  currency: string;
+  amount: number;
+}
+
+export interface Contributor {
+  name: string;
+  role: string;
+  bio: string | null;
+  birth_year: number | null;
+  death_year: number | null;
+}
+
 export interface Edition {
   id: number;
   title: string;
   author_name: string;
+  author_bio: string | null;
   price: number;
+  prices: Price[];
   cover: string;
+  cover_name: string | null;
+  cover_artist: string | null;
   description: string | null;
+  categories: string[];
   format: string;
   language: string;
   page_count: number | null;
-  translator: string | null;
+  translator_name: string | null;
+  illustrator: string | null;
+  introduction_writer: string | null;
+  contributors: Contributor[];
   publication_date: string | null;
   isbn: string | null;
   edition_name: string | null;
+  edition_notes: string | null;
   files: string | null;
   samples: Sample[] | null;
 }
@@ -24,10 +46,12 @@ export interface Sample {
 export interface Book {
   id: number;
   title: string;
+  subtitle: string | null;
   author: string;
   book_slug: string;
-  year_published?: number;
-  author_bio?: string | null;
+  original_language: string;
+  original_publication_year: number | null;
+  contributors: Contributor[];
   categories?: string[];
   editions: Edition[];
 }
@@ -35,6 +59,11 @@ export interface Book {
 export interface BookListItem {
   id: number;
   title: string;
+  subtitle: string | null;
   author: string;
   book_slug: string;
+  original_language: string;
+  original_publication_year: number | null;
+  contributors: Contributor[];
+  editions: Edition[];
 }
