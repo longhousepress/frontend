a6fc58ba80b264bd8d5288e46434223ff38264d4
longhousepress <info@longhousepress.org>
2026-02-09 05:01:59 +0000

    Slight refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({  vite: {    plugins: [tailwindcss()],  },});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
