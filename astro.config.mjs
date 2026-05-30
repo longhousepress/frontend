903cacf28461c59bbce5a38324e8b72e0197082c
longhousepress <info@longhousepress.org>
2026-05-30 02:57:53 +0100

    Add outdir via env var

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL || 'https://longhousepress.org',
+  outDir: process.env.OUT_DIR || './dist',
   build: {
     assets: '_astro',
     assetsPrefix: process.env.CDN_URL || undefined,
